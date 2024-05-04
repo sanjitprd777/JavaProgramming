d8b37631a8b4eefcbb66c2d62c78295a505f9368
sanjitprd777 <sanjitpd777@gmail.com>
2024-05-04 16:01:47 +0530

    Exchanger

---
+++ src/MultiThreading/ExchangerEx/FutureTaskEx1.java
@@ -0,0 +1,31 @@
+package MultiThreading.ExchangerEx;
+
+import java.util.concurrent.Exchanger;
+
+public class FutureTaskEx1 {
+
+    /*
+    To exchange data between threads, we can use Exchanger service.
+    When exchanger calls the exchange method, it waits for another thread to come to this point, then it exchanges the
+    data and proceeds with execution of both threads.
+     */
+    public static void main(String[] args) {
+        int t = 2;
+        Exchanger<Integer> exchanger = new Exchanger<>();
+
+        Thread[] th = new Thread[t];
+        for (int i = 0; i < t; ++i) {
+            th[i] = new CustomThread(exchanger);
+            th[i].start();
+        }
+
+        for (int i = 0; i < t; ++i) {
+            try {
+                th[i].join();
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+    }
+}
