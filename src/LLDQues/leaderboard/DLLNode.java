c7af2989adde0a08dba35eb47724fb2c7d988f42
sanjitprd777 <sanjitpd777@gmail.com>
2024-06-10 19:57:32 +0530

    LeaderBoard

---
+++ src/LLDQues/leaderboard/DLLNode.java
@@ -0,0 +1,22 @@
+package LLDQues.leaderboard;
+
+public class DLLNode {
+
+    int count;
+    String name;
+    String country;
+    DLLNode prev;
+    DLLNode next;
+
+    public DLLNode(String name, String country) {
+        this.count = 1;
+        this.name = name;
+        this.country = country;
+        this.prev = null;
+        this.next = null;
+    }
+
+    public void incCount() {
+        this.count++;
+    }
+}
